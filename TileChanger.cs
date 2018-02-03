daa8023ddb52c051c24e2eb91c716aff19e7d43a
xShirohi <edwardk6@uci.edu>
2018-02-03 02:39:31 -0800

    Add files via upload

---
+++ TileChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TileChanger : MonoBehaviour {
+
+    public Button button;
+    public Text colors;
+
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+}
